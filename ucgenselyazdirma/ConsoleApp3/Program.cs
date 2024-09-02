d3a60b7ca74139845ab5c9b23bd21ea79cc7809d
Ozgur <huozce@gmail.com>
2024-09-02 16:57:06 +0300

    ucgensel yazdirma eklendi.

---
+++ ucgenselyazdirma/ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+int parameterx = int.Parse(Console.ReadLine());
+string yildiz = "";
+for  (int i = 0; i < parameterx; i++)
+{
+    
+    yildiz = yildiz + "a";
+    Console.WriteLine(yildiz);
+    
+}for (int y=0; y<parameterx; y++ )
+    {
+        yildiz=yildiz.Remove(parameterx-y);
+        Console.WriteLine(yildiz);
+    }
