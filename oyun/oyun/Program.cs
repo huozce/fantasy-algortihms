1a2de8be259f29d2ea349dc03ba0ea309df5f483
Ozgur <huozce@gmail.com>
2024-09-02 11:25:19 +0300

    first commit

---
+++ oyun/oyun/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+
+Random random = new Random();
+int hasar = random.Next(0, 100);
+
+
+
+Console.WriteLine("İsim Giriniz.");
+string isim = Console.ReadLine();
+oyuncu oyuncu1 = new oyuncu(isim,0);
+
+Console.WriteLine("İsim Giriniz.");
+string isim2 = Console.ReadLine();
+oyuncu oyuncu2 = new oyuncu(isim2,1);
+
+
+
+
+int y = 0;
+while (oyuncu1.Can >= 0 && oyuncu2.Can >= 0)
+{
+    string x = Console.ReadLine();
+    oyunbasla(x);
+}
+if (oyuncu1.Can <= 0)
+{
+    Console.WriteLine(isim + "kaybettin haci");
+}
+if (oyuncu2.Can <= 0)
+{ Console.WriteLine(isim2 + "kaybettin haci"); }
+
+
+
+void oyunbasla(string x)
+{
+    hasar = random.Next(0, 100);
+    if (y % 2 == 0)
+    {
+        Console.WriteLine("Sıra " + isim + "de");
+        if (x == "vur")
+        {
+            
+            oyuncu2.Can = oyuncu2.Can - hasar;
+            if (oyuncu2.Can >= 100)
+            { oyuncu2.Can = 100; }
+            Console.WriteLine(isim2 + "in cani" + oyuncu2.Can + isim + "in cani" + oyuncu1.Can);
+        }
+        else if (x == "can")
+        {
+           
+            oyuncu1.Can = oyuncu1.Can + hasar;
+            if (oyuncu1.Can >= 100)
+            { oyuncu1.Can = 100; }
+            Console.WriteLine(isim2 + "  in cani  " + oyuncu2.Can + isim + "  in cani  " + oyuncu1.Can);
+        }
+
+    }
+    else if (y % 2 == 1)
+    {
+        Console.WriteLine("Sıra " + isim2 + "de");
+        if (x == "vur")
+        {
+            
+            oyuncu1.Can = oyuncu1.Can - hasar;
+            if (oyuncu1.Can >= 100)
+            { oyuncu1.Can = 100; }
+            Console.WriteLine(isim2 + "in cani" + oyuncu2.Can + isim + "in cani" + oyuncu1.Can);
+        }
+        else if (x == "can")
+        {
+            
+            oyuncu2.Can = oyuncu2.Can + hasar;
+            if (oyuncu2.Can >= 100)
+            { oyuncu2.Can = 100; }
+            Console.WriteLine(isim2 + "in cani" + oyuncu2.Can + isim + "in cani" + oyuncu1.Can);
+        }
+    }
+    y = y + 1;
+}
+
+
+
+
+
+class oyuncu
+{
+    public int Can = 100;
+
+    public string player1 = "Player 1 ";
+    public string player2 = "Player 2 ";
+   
+    
+
+    public oyuncu(string nick,int i)
+    {
+        
+        if ( i == 0) { 
+        Console.WriteLine(player1 + "in ismi " + nick); ;
+        }
+        
+        else if(i==1) { Console.WriteLine(player2 + "nin ismi " + nick); }
+        
+
+    }
+
+}
---
+++ ucgen/ucgen/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int x=0;
+int y=0;
+int z=0;
+int mutlakA = x - y;
+int mutlakB = x - z;  
+int mutlakC = z - y;
+string xstr = x.ToString();
+string ystr = y.ToString();
+string zstr = z.ToString();
+xstr = Console.ReadLine();
+ystr = Console.ReadLine();
+zstr = Console.ReadLine();
+x=Convert.ToInt32(xstr);
+y=Convert.ToInt32(ystr);
+z=Convert.ToInt32(zstr);
+int xx = Math.Abs(mutlakA);
+int yy = Math.Abs(mutlakB);
+int zz= Math.Abs(mutlakC);
+ucgen(x, y, z);
+void ucgen(int x, int y, int z){
+
+    if (((xx<z)&&(z<x+y))&&((yy<y)&&(y<x+z))&&(((zz<x)&&(x<y+z))))
+    {
+        if ((x * x + y * y == z * z) || (x * x + z * z == y * y) || (z * z + y * y == x * x))//buradaki mantık çok daha fazla azaltılabilir.
+            Console.WriteLine("dik ucgendir");
+        else if (x == y && y == z) { Console.WriteLine("eskenar ucgendir"); }
+        else { Console.WriteLine("ucgendir"); }
+
+    }
+    else { Console.WriteLine("ucgen degildir"); }
+}
